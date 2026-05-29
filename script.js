14e7f9a48462ae1b5b77c99e52a07ea7e595a049
brocthi-wq <brocthi@gmail.com>
2026-05-29 12:57:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
    1. Initialisation de Firebase
 ========================================================== */
 const firebaseConfig = {
-    apiKey: "VOTRE_API_KEY",
-    authDomain: "votre-projet.firebaseapp.com",
-    projectId: "votre-projet-id",
-    storageBucket: "votre-projet.appspot.com",
-    messagingSenderId: "1234567890",
-    appId: "1:1234567890:web:abcdefghijk"
+   apiKey: "AIzaSyCr1B9DpzHwktKKzaV5-buKcibs_4EXJKk",
+  authDomain: "pokedex-17938.firebaseapp.com",
+  projectId: "pokedex-17938",
+  storageBucket: "pokedex-17938.firebasestorage.app",
+  messagingSenderId: "268509858726",
+  appId: "1:268509858726:web:1a644d77d3affc8d619d67",
+  measurementId: "G-3GZ307S10L"
 };
 firebase.initializeApp(firebaseConfig);
 
